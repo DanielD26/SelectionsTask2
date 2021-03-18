316cbd11bf7a92d91ff450056c0ab49ac364925a
DanielD26 <103547416@student.swin.edu.au>
2021-03-18 19:39:38 +1100

    Exericse 2

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SelectionsTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] username = {"Daniel, Naruto"};
+            string[] password = {"Potato, Ramen"};
+
+            Console.WriteLine("Please enter username: ");
+            string usernameResult = Console.ReadLine();
+
+            string userDaniel = "Daniel";
+            string DanielResult = Array.Find(username, element => element == userDaniel); 
+            
+            if (usernameResult == DanielResult) {
+                Console.WriteLine("User exists");
+            } else {
+                Console.WriteLine("User doesn't exist");
+            }
+        }
+    }
+}
